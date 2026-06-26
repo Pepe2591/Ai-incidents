ed33c9e21582dbb9a026a4a8e0e03f56be0ceacc
Pepe2591 <noucub234@gmail.com>
2026-06-26 19:58:07 +0600

    fixed display

--- ai_incidents/accidents/models.py
+++ ai_incidents/accidents/models.py
@@ -11,13 +11,20 @@ class Camera(models.Model):
     width = models.FloatField()
     longitude = models.FloatField()
 
+    def __str__(self):
+        return f"name: {self.name} status: {self.status}"
+
 class Category(models.Model): 
     name = models.CharField(max_length=100)
     desc = models.TextField()
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
+    def __str__(self):
+        return f"name: {self.name}, desc: {self.desc}" 
+
 class Dangerous_event(models.Model):
+    name = models.CharField(max_length=100)
     url_img = models.TextField()
     url_video = models.TextField()
     event_status = models.CharField()
@@ -29,3 +36,6 @@ class Dangerous_event(models.Model):
     conf = models.FloatField()
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
+
+    def __str__(self):
+        return f"name: {self.name}, event_status: {self.event_status}, created_at: {self.created_at}"
