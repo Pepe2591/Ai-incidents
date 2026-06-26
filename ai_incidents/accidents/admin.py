2dd3f9845fb36e82edab2fab9a16b814e688dccc
Pepe2591 <noucub234@gmail.com>
2026-06-26 19:43:53 +0600

    added models reg on site

--- ai_incidents/accidents/admin.py
+++ ai_incidents/accidents/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Camera, Dangerous_event, Category
 
 # Register your models here.
+
+admin.site.register(Camera)
+admin.site.register(Dangerous_event)
+admin.site.register(Category)
