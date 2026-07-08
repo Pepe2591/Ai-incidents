d9eedfa891d77c4afb00ad7c5f399a2fa63b2ca7
Pepe2591 <noucub234@gmail.com>
2026-07-08 14:11:02 +0600

    Fixed relationship models

--- ai_incidents/accidents/models.py
+++ ai_incidents/accidents/models.py
@@ -1,18 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
 
-class Camera(models.Model):
-    status = models.BooleanField()
-    name = models.CharField(max_length=100)
-    address = models.CharField(max_length=400)
-    stream_url = models.TextField()
-    cur_event_id = models.ForeignKey('Dangerous_event', related_name = 'Camera', on_delete = models.CASCADE)
-    width = models.FloatField()
-    longitude = models.FloatField()
-
-    def __str__(self):
-        return f"name: {self.name} status: {self.status}"
 
 class Category(models.Model): 
     name = models.CharField(max_length=100)
@@ -22,20 +12,34 @@ class Category(models.Model):
 
     def __str__(self):
         return f"name: {self.name}, desc: {self.desc}" 
-
+    
 class Dangerous_event(models.Model):
     name = models.CharField(max_length=100)
+    categorys = models.ManyToManyField('Category', related_name='dangerous_events')
+    priority = models.IntegerField()
     url_img = models.TextField()
     url_video = models.TextField()
     event_status = models.CharField()
-    event_type_id = models.ForeignKey(Category, related_name='Dangerous_event', on_delete=models.CASCADE)
-    width = models.FloatField()
+    latitude = models.FloatField()
     longitude = models.FloatField()
-    priority = models.IntegerField()
-    promt = models.TextField()
-    conf = models.FloatField()
+    desc = models.TextField()
+    conf = models.FloatField() 
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
     def __str__(self):
         return f"name: {self.name}, event_status: {self.event_status}, created_at: {self.created_at}"
+
+class Camera(models.Model):
+    status = models.BooleanField()
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=400)
+    stream_url = models.TextField()
+    width = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return f"name: {self.name} status: {self.status}"
+
+
+
