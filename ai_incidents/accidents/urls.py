315d10acc8be2f91d6c9d13dca7b1844356824e4
Pepe2591 <noucub234@gmail.com>
2026-07-13 16:02:52 +0600

    updated register router

--- ai_incidents/accidents/urls.py
+++ ai_incidents/accidents/urls.py
@@ -4,13 +4,8 @@ from .views import (CameraModelViewSet, FiresModelViewSet, ExplosionModelViewSet
                     CollapseMidelViewSet, CoreSericeViewSet, LogsViewSet, AnalysisViewSet)
 
 router = DefaultRouter()
-router.register(r'cameras', CameraModelViewSet, basename='camera')
-router.register(r'fires', FiresModelViewSet, basename='fire')
-router.register(r'explosions', ExplosionModelViewSet, basename='explosion')
-router.register(r'collapse', CollapseMidelViewSet, basename='collapse')
-router.register(r'core-service', CoreSericeViewSet, basename='core-service')
-router.register(r'logs', LogsViewSet, basename='log')
-router.register(r'analysis', AnalysisViewSet, basename='analysis')
+router.register(r'cameras', CameraModelViewSet, basename='camera') 
+
 
 
 urlpatterns = [
