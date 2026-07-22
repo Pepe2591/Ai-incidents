9a3629c9c9cad57f495a07c6ac2d4c25989bd111
Pepe2591 <noucub234@gmail.com>
2026-07-22 13:39:29 +0600

    Deleted register models

--- ai_incidents/accidents/admin.py
+++ ai_incidents/accidents/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
-from .models import Camera, Event, Category
 
 # Register your models here.
 
-admin.site.register(Camera)
-admin.site.register(Event)
-admin.site.register(Category)
