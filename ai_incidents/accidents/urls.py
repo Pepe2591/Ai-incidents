70803d1c2bd44d7b402a3dd07bf7481c60c5b379
Pepe2591 <noucub234@gmail.com>
2026-07-22 12:47:10 +0600

    New router

--- ai_incidents/accidents/urls.py
+++ ai_incidents/accidents/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from events.views import 
+from events.views import AccidentModelViewSet
 
 router = DefaultRouter()
-router.register(r'cameras', CameraModelViewSet, basename='camera') 
+router.register(r'accidents', AccidentModelViewSet, basename='accident') 
 
 
 urlpatterns = [
