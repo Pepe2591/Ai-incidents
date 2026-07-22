297131b21dd10d20c976b5e34d02ab335079a53b
Pepe2591 <noucub234@gmail.com>
2026-07-22 13:42:16 +0600

    Registr new models

--- ai_incidents/events/admin.py
+++ ai_incidents/events/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Event, Frame, Camera
 
 # Register your models here.
+
+admin.site.register(Event)
+admin.site.register(Frame)
+admin.site.register(Camera)
