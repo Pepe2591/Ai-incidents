3da0f2df1819625085476c39bee641b11bb3e9bb
Pepe2591 <noucub234@gmail.com>
2026-07-22 12:31:13 +0600

    Added new path

--- ai_incidents/collapse/urls.py
+++ ai_incidents/collapse/urls.py
@@ -1,1 +1,10 @@
-from rest_framework.serializers import ModelSerializer 
+from rest_framework.routers import DefaultRouter
+from accidents.views import CollapseModelViewSet
+from django.urls import path, include
+
+router = DefaultRouter()
+router.register('collapses', CollapseModelViewSet, 'collapse')
+
+urlpatterns = [
+    path('', include(router.urls))
+]
