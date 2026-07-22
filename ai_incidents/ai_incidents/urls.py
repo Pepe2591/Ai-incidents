0185c6c09dedbaf7c00eace275fab29483b2b448
Pepe2591 <noucub234@gmail.com>
2026-07-22 14:33:29 +0600

    Fixed path

--- ai_incidents/ai_incidents/urls.py
+++ ai_incidents/ai_incidents/urls.py
@@ -36,6 +36,11 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls), 
     path('api/', include('accidents.urls')), 
+    path('api/', include('collapse.urls')),
+    path('api/', include('explosions.urls')),
+    path('api/', include('fires.urls')),
+    path('api/', include('events.urls')),
+    path('api/', include('logs.urls')),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name = 'schema-json'), 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name = 'schema-swagger-ui'), 
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name = 'schema-redoc')
